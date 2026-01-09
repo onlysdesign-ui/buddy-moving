215a2f35b63b158988973fb00fc66f8be66d2229
onlysdesign-ui <onlysdesign@gmail.com>
2026-01-09 23:24:43 +0300

    Build docs for updated tests UI

--- frontend/src/tests/testCases.ts
+++ frontend/src/tests/testCases.ts
@@ -1,7 +1,7 @@
 import rawCases from "./eval_cases_v2.json";
 import type { TestContext } from "./types";
 
-export const testContexts = rawCases as TestContext[];
+export const defaultTestContexts = rawCases as TestContext[];
 
-export const getTotalCases = () =>
-  testContexts.reduce((total, context) => total + context.cases.length, 0);
+export const getTotalCases = (contexts: TestContext[] = defaultTestContexts) =>
+  contexts.reduce((total, context) => total + context.cases.length, 0);
